e99e591d05a311fa955296ef44034f73f9f8855e
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2025-05-02 12:41:46 +0530

    commit

---
+++ config/config.py
@@ -0,0 +1,3 @@
+GEMINI_API_KEY = "AIzaSyDllhh8PF8P27IFP6B4E-----------**"  # Replace with your actual Gemini API key i have added only half of my key for security reasons.
+MONGODB_URI = "mongodb://localhost:27017/"
+DB_NAME = "edu_chatbot"
