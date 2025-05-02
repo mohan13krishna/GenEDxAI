8bace39c4f4f11a66a4223404a549c5d3585c44a
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2025-05-02 13:56:19 +0530

    Delete config directory

--- config/config.py
+++
@@ -1,3 +0,0 @@
-GEMINI_API_KEY = "AIzaSyDllhh8PF8P27IFP6B4E-----------**"  # Replace with your actual Gemini API key i have added only half of my key for security reasons.
-MONGODB_URI = "mongodb://localhost:27017/"
-DB_NAME = "edu_chatbot"
