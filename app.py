1af250efb8351aedc20e126a38b860aef7951793
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2025-05-29 13:56:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
  
 # import streamlit as st
-# import google.generativeai as genai
+# import google.generativeai as genai 
 # from utils.auth import login_user, register_user 
 # from utils.chatbot import get_learning_response    
 # from utils.exam import generate_exam, evaluate_exam  
